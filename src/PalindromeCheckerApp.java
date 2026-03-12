6d6220127d57735f056b13ece9ad29758157c620
Harsha <harshabandaru2410@gmail.com>
2026-03-13 00:31:23 +0530

    UC1 - Welcome message

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,41 +1,9 @@
-import java.util.Scanner;
-
 public class PalindromeCheckerApp {
 
     public static void main(String[] args) {
 
-        // UC1
         System.out.println("Welcome to Palindrome Checker App");
         System.out.println("Application Version: 1.0");
 
-        // UC2
-        String word = "madam";
-        String reversed = "";
-
-        for(int i = word.length()-1; i>=0; i--)
-            reversed += word.charAt(i);
-
-        if(word.equals(reversed))
-            System.out.println(word + " is a Palindrome");
-        else
-            System.out.println(word + " is not a Palindrome");
-
-        // UC3
-        Scanner sc = new Scanner(System.in);
-
-        System.out.print("Enter a string: ");
-        String input = sc.nextLine();
-
-        String rev = "";
-
-        for(int i=input.length()-1;i>=0;i--)
-            rev += input.charAt(i);
-
-        if(input.equals(rev))
-            System.out.println("Palindrome");
-        else
-            System.out.println("Not Palindrome");
-
-        sc.close();
     }
 }
