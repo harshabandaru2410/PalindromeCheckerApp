465e56194771a2a0eaedc8421ea71f7183202aed
Harsha <harshabandaru2410@gmail.com>
2026-03-13 00:42:38 +0530

    UC3

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -4,25 +4,20 @@ public class PalindromeCheckerApp {
 
     public static void main(String[] args) {
 
-        // UC1
         System.out.println("Welcome to Palindrome Checker App");
         System.out.println("Application Version: 1.0");
 
-        // UC2 - Hardcoded palindrome
         String word = "madam";
         String reversed = "";
 
-        for (int i = word.length() - 1; i >= 0; i--) {
+        for (int i = word.length() - 1; i >= 0; i--)
             reversed += word.charAt(i);
-        }
 
         if (word.equals(reversed))
             System.out.println(word + " is a Palindrome");
         else
             System.out.println(word + " is not a Palindrome");
 
-
-        // UC3 - User input palindrome using reverse
         Scanner sc = new Scanner(System.in);
 
         System.out.print("Enter a string to check palindrome: ");
@@ -30,38 +25,12 @@ public class PalindromeCheckerApp {
 
         String rev = "";
 
-        for (int i = input.length() - 1; i >= 0; i--) {
+        for (int i = input.length() - 1; i >= 0; i--)
             rev += input.charAt(i);
-        }
 
-        if (input.equals(rev))
-            System.out.println("Palindrome (Using Reverse)");
-        else
-            System.out.println("Not a Palindrome (Using Reverse)");
-
-
-        // UC4 - Character Array Palindrome Check
-        char[] arr = input.toCharArray();
-
-        int start = 0;
-        int end = arr.length - 1;
-        boolean isPal = true;
-
-        while (start < end) {
-
-            if (arr[start] != arr[end]) {
-                isPal = false;
-                break;
-            }
-
-            start++;
-            end--;
-        }
-
-        if (isPal)
-            System.out.println("Palindrome (Using Character Array)");
-        else
-            System.out.println("Not a Palindrome (Using Character Array)");
+        System.out.println(input.equals(rev)
+                ? "Palindrome"
+                : "Not a Palindrome");
 
         sc.close();
     }
