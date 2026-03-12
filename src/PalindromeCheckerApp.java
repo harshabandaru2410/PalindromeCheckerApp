da3ca8870516d114b218569dc09bb1b2c2567f9b
Harsha <harshabandaru2410@gmail.com>
2026-03-13 00:31:50 +0530

    UC2 - Hardcoded palindrome

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -2,8 +2,22 @@ public class PalindromeCheckerApp {
 
     public static void main(String[] args) {
 
+        // UC1
         System.out.println("Welcome to Palindrome Checker App");
         System.out.println("Application Version: 1.0");
+
+        // UC2
+        String word = "madam";
+        String reversed = "";
+
+        for(int i = word.length() - 1; i >= 0; i--) {
+            reversed += word.charAt(i);
+        }
+
+        if(word.equals(reversed))
+            System.out.println(word + " is a Palindrome");
+        else
+            System.out.println(word + " is not a Palindrome");
 
     }
 }
